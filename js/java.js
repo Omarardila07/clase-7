96557d40e7fdbe0f8a39439700afdd5b5a19ccd8
Omarardila07 <ardilaomar848@gmail.com>
2023-09-16 12:13:10 -0500

    calculador3

--- js/java.js
+++ js/java.js
@@ -2,37 +2,50 @@ $ (".suma").click(function () {
 
     let var1 = parseInt($(".pepito").val());
     let var2 = parseInt($(".nombre").val());
-
-    $(".resultado").html(var1 + var2) 
+    let resultado=suma (var1,var2);
+    $(".resultado").html(resultado) ;
 
 });
+
+function suma(a,b){
+    return a+b;
+}
 
 $ (".resta").click(function () {
 
     let var1 = parseInt($(".pepito").val());
     let var2 = parseInt($(".nombre").val());
-
-    $(".resultado1").html(var1 - var2) 
-
+    let resultado=resta (var1,var2);
+    $(".resultado1").html(resultado) ;
 });
+
+function resta(a,b){
+    return a-b;
+}
 
 $ (".multiplicacion").click(function () {
 
     let var1 = parseInt($(".pepito").val());
     let var2 = parseInt($(".nombre").val());
-
-    $(".resultado2").html(var1 * var2) 
-
+    let resultado=multiplicacion (var1,var2);
+    $(".resultado2").html(resultado) ;
 });
+
+function multiplicacion(a,b){
+    return a*b;
+}
 
 $ (".divicion").click(function () {
 
     let var1 = parseInt($(".pepito").val());
     let var2 = parseInt($(".nombre").val());
-
-    $(".resultado3").html(var1 / var2) 
-
+    let resultado=divicion (var1,var2);
+    $(".resultado3").html(resultado) ;
 });
+
+function divicion(a,b){
+    return a/b;
+}
 
 
 
